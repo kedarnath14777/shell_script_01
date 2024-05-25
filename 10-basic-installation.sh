603f4f09d2755467e9752fa63c9507724e935a6a
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 22:56:32 +0530

    hello

--- 10-basic-installation.sh
+++ 10-basic-installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-user=$(id-u)
+user=$(id -u)
 
-if [ $user -eq 0 ]
+if [ $user -ne 0 ]
 then 
  echo "please run the scripts with super user "
  exit # if the user is not super user exiting the script 
