0f3c0572374fbe255ea16d7504f345b400bd59a3
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 22:18:00 +0530

    hello

---
+++ 08-Special-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# to print all arguments is 
+echo "arg ::$@"
+# no of arg passed 
+echo "arg :: S#"
+
+# to know script nam e
+echo  "script name ::$0"
+
+# to know pwd 
+echo " current working directory is :: $PWD" 
