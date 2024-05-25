7d7e1a9ce8d2938113971b2c16a4a412ec2ce6bd
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 23:01:03 +0530

    hello

--- 10-basic-installation.sh
+++ 10-basic-installation.sh
@@ -5,10 +5,13 @@ user=$(id -u)
 if [ $user -ne 0 ]
 then 
  echo "please run the scripts with super user "
- exit # if the user is not super user exiting the script 
+ exit 1 # if the user is not super user exiting the script 
 else
  echo "ypur super user  " 
 fi 
 
 
 dnf install mysql -y 
+
+
+echo "
