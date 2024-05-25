bfc90acb999926ac2630f842af5c302eb9b73ce1
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 22:25:43 +0530

    hello

--- 08-Special-variables.sh
+++ 08-Special-variables.sh
@@ -3,10 +3,25 @@
 # to print all arguments is 
 echo "arg ::$@"
 # no of arg passed 
-echo "arg :: S#"
+echo "arg :: $#"
 
 # to know script nam e
 echo  "script name ::$0"
 
 # to know pwd 
 echo " current working directory is :: $PWD" 
+
+# to know the current user 
+echo "current user:: $USER"
+# to know the home directory of the user 
+echo "home directory of the user is :: $HOME"
+# to know the hostname
+echo " hostname is :: $HOSTUSER"
+# get the current process id of the shell script 
+echo " pis of the shell is :$$"
+#to  know  the last process is 
+sleep 200 
+echo " pid of last runnunig command is ::$!"
+# exit status of the command is 
+echo " exit status of the last commad is :: $?"
+ 
