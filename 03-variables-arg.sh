86126d6706da57c247cfc335518ec5bb46dfae6b
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 19:23:34 +0530

    hello

---
+++ 03-variables-arg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+#to pass h=teh values as arguments we need to use the $ symble at the variable 
+
+person1=$1
+person2=$2 
+
+echo "$person1 :: Hi $person2 , How are you"
+echo "$person2 :: Good $person1 , What about you"
+echo "$person1 :: $person2 How is perparation going"
+echo "$person2 :: $person1 Good am upgrading DevOps AWS Cloud"
+
