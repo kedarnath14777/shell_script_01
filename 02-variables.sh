d88a09ebbd1502f81998bf71a3f1a28de9def2ac
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 19:21:01 +0530

    hello

---
+++ 02-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#variables : we use the reuseble valus in the script 
+#declertive variables 
+
+person1=kiran
+person2=charan 
+
+echo "$person1 :: Hi $person2 , How are you"
+echo "$person2 :: Good $person1 , What about you"
+echo "$person1 :: $person2 How is perparation going"
+echo "$person2 :: $person1 Good am upgrading DevOps AWS Cloud"
