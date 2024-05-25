d4855b4924a2f6a4dce02e8005e71c0e8f5a35d6
kedarnath3699 <anubolukedarnath14777@gmsil.com>
2024-05-25 22:12:43 +0530

    hello

--- 06-data-type.sh
+++ 06-data-type.sh
@@ -5,13 +5,13 @@
 number1=$1
 number2=$2
 
-sum=$number1+$number2
+sum=$(($number1+$number2))
 echo "sum of 2 is :: $sum"
 
-sub=$number2-$number1
+sub=$(($number2-$number1))
 echo "sub of 2 is :: $sub"
-mul=$number1*$number2
+mul=$(($number1*$number2))
 echo "mul of 2 is :: $mul"
-div=$number1/$number2
+div=$(($number1/$number2))
 echo "div of 2 is :: :: $div"
 
